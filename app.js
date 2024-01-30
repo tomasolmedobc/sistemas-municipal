c973f89819cdf63392b74c2da572c2010a3044c7
Tomas Olmedo <tomasolmedobc@gmail.com>
2024-01-30 09:18:02 -0300

    Se creo una nueva rama

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-// app.js
+/*// app.js
 const express = require('express');
 const mongoose = require('mongoose');
-const db = require('./config/db'); // Asegúrate de que este archivo exporta correctamente la función connectToDatabase
+const db = require('./config/db'); //Conecta a la base de datos
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -35,4 +35,41 @@ app.use('/', impresorasRouter);
 
 app.listen(3000, () => {
     console.log('Server Up! en http://localhost:3000');
+});
+*/
+const express = require('express');
+const mongoose = require('mongoose');
+const db = require('./config/db'); // Conecta a la base de datos
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Llamamos a la función para conectar
+db.connectToDatabase();
+
+// Manejar el evento SIGINT para cerrar la conexión a la base de datos antes de apagar la aplicación
+process.on('SIGINT', async () => {
+    try {
+        await mongoose.connection.close();
+        console.log('Conexión a la base de datos cerrada. Aplicación apagada.');
+        process.exit(0);
+    } catch (error) {
+        console.error('Error al cerrar la conexión a la base de datos:', error.message);
+        process.exit(1);
+    }
+});
+
+app.use('/resources', express.static(__dirname + '/public'));
+
+const impresorasRouter = require('./routes/impresoras');
+app.use('/', impresorasRouter);
+
+const PORT = 3000;
+const HOST = '0.0.0.0'; // Escucha en todas las interfaces de red
+
+app.listen(PORT, HOST, () => {
+    console.log(`Server Up! en http://${HOST}:${PORT}`);
 });
